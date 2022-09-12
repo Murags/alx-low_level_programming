df02779c636ed910dbc1c9f6d0baee78262c8565
Dennis Murage <dmurage140@gmail.com>
2022-09-12 10:24:30 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int count;
+	char lower;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (lower = 'a'; lower <= 'z'; lower++)
+		{
+			_putchar(lower);
+		}
+		_putchar('\n');
+	}
+}
