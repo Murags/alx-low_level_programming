8af61f744091d41c84944ff0d2df5d53a7f5213f
Dennis Murage <dmurage140@gmail.com>
2022-09-12 18:01:31 +0000

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,17 @@
 #include "main.h"
-
+/**
+*print_last_digit - output last digit
+*
+*@num: integer parameter
+*
+*Return: lastDigit
+*/
 int print_last_digit(int num)
 {
 	int lastDigit;
 
 	lastDigit = num % 10;
-	if(lastDigit >= 0)
+	if (lastDigit >= 0)
 	{
 		_putchar(lastDigit + 48);
 		return (lastDigit);
