e2a8a7d30e8a724242b1371c9790e881bc4757ed
Dennis Murage <dmurage140@gmail.com>
2022-09-19 10:51:24 +0000

    Fixing error

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,17 +6,20 @@
 */
 void puts2(char *str)
 {
-	_putchar(*str);
-	str++;
-	str++;
-	while (*str != '\0')
+	if(*str != '\0')
 	{
-		str--;
-		if (*str != '\0')
-		{
-		str++;
 		_putchar(*str);
-		str += 2;
+		str++;
+		str++;
+		while (*str != '\0')
+		{
+			str--;
+			if (*str != '\0')
+			{
+				str++;
+				_putchar(*str);
+				str += 2;
+			}
 		}
 	}
 	_putchar('\n');
