f98424729b7472cd90fc6a72d72b3e6fc28f90e5
Dennis Murage <dmurage140@gmail.com>
2022-09-19 10:44:30 +0000

    Fixing errors

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,10 +6,18 @@
 */
 void puts2(char *str)
 {
+	_putchar(*str);
+	str++;
+	str++;
 	while (*str != '\0')
 	{
+		str--;
+		if (*str != '\0')
+		{
+		str++;
 		_putchar(*str);
 		str += 2;
+		}
 	}
 	_putchar('\n');
 }
