a2004421e166c1af2691619fbbe48d2a169675c3
Dennis Murage <dmurage140@gmail.com>
2022-09-22 08:13:42 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+*rot13 - encodes with rot13
+*
+*@str: string parameter
+*
+*Return: encoded string
+*/
+char *rot13(char *str)
+{
+	int i;
+	char temp;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
+		{
+			if (*(str + i) > 'm')
+			{
+				temp = *(str + i) - 'm';
+				*(str + i) = temp + 96;
+			}
+			else
+			{
+				*(str + i) += 13;
+			}
+		}
+		else if (*(str + i) >= 'A' && *(str + i) <= 'Z')
+		{
+			if (*(str + i) > 'M')
+			{
+				temp = *(str + i) - 'M';
+				*(str + i) = temp + 64;
+			}
+			else
+			{
+				*(str + i) += 13;
+			}
+		}
+	}
+	return (str);
+}
