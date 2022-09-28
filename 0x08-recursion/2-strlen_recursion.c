891a5b285af59e9f988f956b16fd3f9426fc47c3
Dennis Murage <dmurage140@gmail.com>
2022-09-28 08:26:13 +0000

    string length with recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_strlen_recursion - calculates string length
+*
+*@s: pointer to first character in string
+*
+*Return: stirng length
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(++s));
+	}
+}
