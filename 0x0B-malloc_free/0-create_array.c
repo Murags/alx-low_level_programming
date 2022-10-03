86274f123cb20fe70a05f6e7f0fde8e3361d9f99
Dennis Murage <dmurage140@gmail.com>
2022-10-03 10:09:15 +0000

    function that creates an array of chars, and initializes it with a specific char.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,6 +21,10 @@ char *create_array(unsigned int size, char c)
 	else if (size > 0)
 	{
 		ptr = malloc(sizeof(char) * size);
+		if (ptr == '\0')
+		{
+			return ('\0');
+		}
 		for (i = 0; i < size; i++)
 		{
 			ptr[i] = c;
