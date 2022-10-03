0ef30d93cb67fa020aad186f198a97ffe309f95c
Dennis Murage <dmurage140@gmail.com>
2022-10-03 10:19:56 +0000

    Fixing compilation errors

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,20 +18,17 @@ char *create_array(unsigned int size, char c)
 	{
 		return ('\0');
 	}
+	ptr = malloc(sizeof(char) * size);
+	if (ptr == '\0')
+	{
+		return ('\0');
+	}
 	else
 	{
-		ptr = malloc(sizeof(char) * size);
-		if (ptr == '\0')
+		for (i = 0; i < size; i++)
 		{
-			return ('\0');
+			ptr[i] = c;
 		}
-		else
-		{
-			for (i = 0; i < size; i++)
-			{
-				ptr[i] = c;
-			}
-			return (ptr);
-		}
+		return (ptr);
 	}
 }
