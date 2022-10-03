ddb709cc6a4afb746d4776971515c8f425c8996b
Dennis Murage <dmurage140@gmail.com>
2022-10-03 13:03:18 +0000

    concatinating two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,9 +26,11 @@ char *str_concat(char *s1, char *s2)
 		return ("");
 	}
 	for (len1 = 0; *(s1 + len1); len1++)
-
+	{
+	}
 	for (len2 = 0; *(s2 + len2); len2++)
-
+	{
+	}
 	concat = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (concat == NULL)
 	{
