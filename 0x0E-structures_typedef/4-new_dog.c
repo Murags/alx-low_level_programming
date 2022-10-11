991781547213d2edfe3989f66ad7f25bfa2aaccf
Dennis Murage <dmurage140@gmail.com>
2022-10-11 14:20:25 +0000

    Handling error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,19 +11,44 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t snoppdg, *temp;
-	char *tempName = name, *tempOwner = owner;
+	dog_t *snoppdg;
+	int lenName, lenOwner, i;
 
-	if (name == NULL || owner == NULL || age < 0)
+	snoppdg = malloc(sizeof(dog_t));
+	if (snoppdg == NULL)
+	{
+		free(snoppdg);
 		return (NULL);
+	}
+	for (lenName = 0; name[lenName] != '\0'; lenName++)
 
-	snoppdg.name = tempName;
-	snoppdg.age = age;
-	snoppdg.owner = tempOwner;
+	for (lenOwner = 0; owner[lenOwner] != '\0'; lenOwner++)
 
-	temp = &snoppdg;
-	if (temp == NULL)
+	snoppdg->name = malloc(sizeof(char) * (lenName + 1));
+	if (snoppdg->name == NULL)
+	{
+		free(snoppdg->name);
+		free(snoppdg);
 		return (NULL);
+	}
+	for (i = 0; i < lenName; i++)
+		snoppdg->name[i] = name[i];
 
-	return (temp);
+	snoppdg->name[i] = '\0';
+
+	snoppdg->age = age;
+
+	snoppdg->owner = malloc(sizeof(char) * (lenOwner + 1));
+	if (snoppdg->owner == NULL)
+	{
+		free(snoppdg->owner);
+		free(snoppdg->name);
+		free(snoppdg);
+		return (NULL);
+	}
+	for (i = 0; i < lenOwner; lenOwner++)
+		snoppdg->owner[i] = owner[i];
+
+	snoppdg->owner[i] = '\0';
+	return (snoppdg);
 }
