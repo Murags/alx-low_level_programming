9114003ef07bb860852e3ba8e4f7745de67ffa8f
Dennis Murage <dmurage140@gmail.com>
2022-10-11 09:14:43 +0000

    Fixing styling errors

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
+/*
+*dog_t - Typedef for struct dog
+*/
+typedef struct dog dog_t;
 
+/**
+*struct dog - Defines a dog with name, age and owner as elements
+*@name: dog name
+*@age: dog age
+*@owner: dog owner
+*
+*Description: Define a new type of dog
+*/
 struct dog
 {
 	char *name;
@@ -9,7 +21,6 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
