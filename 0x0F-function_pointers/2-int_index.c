2015f5e2581f1c53c05629056a53056aab7090c1
Dennis Murage <dmurage140@gmail.com>
2022-10-13 10:20:00 +0000

    Error handling

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,20 +13,24 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0 ; i < size; i++)
+	if (array != NULL && cmp != NULL)
 	{
-		if (cmp(array[i]) != '\0')
-			break;
+		if (size <= 0)
+			return (-1);
 
-		else if (cmp(array[i]) == '\0')
-			continue;
+		for (i = 0 ; i < size; i++)
+		{
+			if (cmp(array[i]) != '\0')
+				break;
+
+			else if (cmp(array[i]) == '\0')
+				continue;
+		}
+
+		if (i == size)
+			return (-1);
+
+		return (i);
 	}
-
-	if (i == size)
-		return (-1);
-
-	return (i);
+	return (-1);
 }
