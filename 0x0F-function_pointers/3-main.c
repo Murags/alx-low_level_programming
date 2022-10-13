76fdeae71a5d3111df7e06204fd733ba229ec6aa
Dennis Murage <dmurage140@gmail.com>
2022-10-13 11:18:15 +0000

    Error handling

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
-		&& *argv[2] != '/' && *argv[2] != '%')
+		&& *argv[2] != '/' && *argv[2] != '%' && argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
@@ -36,8 +36,6 @@ int main(int argc, char *argv[])
 	}
 
 	result = (int)(get_op_func(argv[2])(num1, num2));
-	if (result == NULL)
-		return (-1);
 
 	printf("%d\n", result);
 	return (0);
