70e3a715320be8649e692c6d64b48d64e9aa9e9f
Dennis Murage <dmurage140@gmail.com>
2022-10-13 19:01:13 +0000

    opscode

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,8 +1,17 @@
 #include "function_pointers.h"
-#include <stdio.h>
-
+#include <stdlib.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
+	char *arr = (char *) main;
+	int i;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
@@ -13,5 +22,15 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(2);
 	}
+	for (i = 0; i < (atoi(argv[1])); i++)
+	{
+		if (i == (atoi(argv[1]) - 1))
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	printf("\n");
 	return (0);
 }
