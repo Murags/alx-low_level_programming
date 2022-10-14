ee7ee6b83e8e007d0753730e5ca4685350584ae0
Dennis Murage <dmurage140@gmail.com>
2022-10-14 08:19:44 +0000

    Variadic function that prints integer arguments passed

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+*print_numbers - print all numbers passed as arguments
+*
+*@separator: separator between the numbers
+*@n: number of arguments
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%d\n", va_arg(ap, unsigned int));
+			break;
+		}
+		printf("%d%s", va_arg(ap, unsigned int), separator);
+	}
+
+	va_end(ap);
+}
