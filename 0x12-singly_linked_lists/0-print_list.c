e76970835a30326c640ce9c838c7a6bc483bfa84
Dennis Murage <dmurage140@gmail.com>
2022-10-24 09:58:33 +0000

    Added 0-print_list.c that prints lists provided

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+*print_list - prints list provided
+*
+*@h: pointer to first element on the list
+*
+*Return: number of elements printed
+*/
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	list_t *current = NULL;
+
+	if (h == NULL)
+		return (-1);
+
+	current = malloc(sizeof(list_t));
+
+	if (current == NULL)
+		return (-1);
+
+	current->str = h->str;
+	current->len = h->len;
+	current->next = h->next;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", current->len, current->str);
+
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
