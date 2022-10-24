9e53fe1ad879d17ed23da4b703769e8569d43c26
Dennis Murage <dmurage140@gmail.com>
2022-10-24 18:59:44 +0000

    Error handling

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,12 @@
-section .text
-global _start
+global	_start
+	extern	printf
 
-section .data
-msg db  'Hello, Holberton',0xa
 
 _start:
-
-    mov ecx,msg ;message to write
-    mov ebx,1   ;file descriptor (stdout)
-    mov eax,4   ;system call number (sys_write)
-    int 0x80    ;call kernel
-
-    mov ebx,0   ;process' exit code
-    mov eax,1   ;system call number (sys_exit)
-    int 0x8
+	mov	rdi, msg
+	mov	rax, 0
+	call	printf
+	mov	rax, 0
+	ret
+msg:
+	db "Hello, Holberton", 10, 0
