d2e607610509e68908ae26eb9d83877eb185d281
Dennis Murage <dmurage140@gmail.com>
2022-10-26 19:33:58 +0000

    Fixing syling

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
-
+/**
+*add_nodeint - adds an element at the beginning
+*
+*@head: pointer to list
+*@n:element to be added
+*
+*Return: new element address
+*/
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
