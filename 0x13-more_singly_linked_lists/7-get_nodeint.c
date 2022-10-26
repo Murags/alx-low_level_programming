5643549c44a211e2593989ae1a419bff997e28df
Dennis Murage <dmurage140@gmail.com>
2022-10-26 20:06:23 +0000

    Handling error

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head == NULL)
+	if (head == NULL && index == '\0')
 		return (NULL);
 
 	while (head)
