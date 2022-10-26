f38d119d173ae5df6a2b70bfb002ef8a61666c24
Dennis Murage <dmurage140@gmail.com>
2022-10-26 20:10:44 +0000

    Handling error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	temp = *head;
 
-	if (head == NULL)
+	if (head == NULL && idx == '\0')
 		return (NULL);
 
 	while (i < (idx - 1) && temp)
