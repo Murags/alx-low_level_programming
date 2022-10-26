6f23d9cba71dfc98e428d8b232b882ce6aed3a87
Dennis Murage <dmurage140@gmail.com>
2022-10-26 07:04:02 +0000

    New directory added with README.md file and project file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+*print_listint -  prints all the elements of a listint_t list.
+*
+*@h: list of elements
+*
+*Return: NUmber of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+	const listint_t *current;
+
+	if (h == NULL)
+		return (0);
+
+	current = malloc(sizeof(listint_t));
+
+	if (current == NULL)
+		return (0);
+
+	current = h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		nodes++;
+		current = current->next;
+	}
+	return (nodes);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+| FileName | Description |
+| :------- | :---------- |
+| `0-print_listint.c` | Function that prints all the elements of a `listint_t` list. |
