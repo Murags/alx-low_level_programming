599fe1b5db72200dfeefc48ea21c3527db196c92
Dennis Murage <dmurage140@gmail.com>
2022-10-27 15:36:21 +0000

    error handle

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			temp = temp->next;
 			i++;
 		}
-		if (temp == NULL)
+		if (temp == NULL || temp->next == NULL)
 			return (-1);
 
 		remove = temp->next;
