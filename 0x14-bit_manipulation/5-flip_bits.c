09c76700b16653013881026c03499beda5bcc98e
Dennis Murage <dmurage140@gmail.com>
2022-10-31 20:20:39 +0000

    Added 5-flip_bits.c project file

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,9 +13,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int different;
 	unsigned int count = 0;
 
-	if (n == 0 || m == 0)
-		return (-1);
-
 	different = n ^ m;
 
 	while (different)
