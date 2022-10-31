4c51651b1ff8a913b469451df65b3447fbdb8224
Dennis Murage <dmurage140@gmail.com>
2022-10-31 19:56:08 +0000

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,10 +9,10 @@
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == 0 || index >= 64)
+	if (n == NULL || index >= 64)
 		return (-1);
 
 	*n = *n & (~(1<<index));
 
-	return (-1);
+	return (1);
 }
