f047e80a53d4ddd089992173bbad60a1c731209f
Dennis Murage <dmurage140@gmail.com>
2022-11-01 10:17:08 +0000

    Handle ULONG_MAX input

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,73 +1,13 @@
 #include "main.h"
 
-unsigned long int _pow(unsigned long int base, unsigned long int power);
 /**
-*_pow - raises base to power
-*
-*@base: integer to raise
-*@power: power to raise
-*
-*Return: Integer raised to a power
-*/
-unsigned long int _pow(unsigned long int base, unsigned long int power)
-{
-	unsigned long int output = 1;
-
-	if (power == 0)
-		return (1);
-
-	while (power > 0)
-	{
-		output *= base;
-		power--;
-	}
-
-	return (output);
-}
-/**
-*print_binary - converts binary to decimal
-*
-*@n: decimal to convert
-*/
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ */
 void print_binary(unsigned long int n)
 {
-	unsigned long int power = 0;
-	unsigned long int rem;
-
-	if (n == 0)
-		_putchar('0');
-
-	while (n >= _pow(2, power))
-		power++;
-
-	if (n < _pow(2, power))
-		power--;
-
-	rem = n;
-	while (power >= 1)
-	{
-		if (rem >= _pow(2, power))
-		{
-			_putchar('1');
-			rem = rem - _pow(2, power);
-			power--;
-		}
-		else
-		{
-			_putchar('0');
-			power--;
-		}
-	}
-	if (rem >= _pow(2, power))
-                {
-                        _putchar('1');
-                        rem = rem - _pow(2, power);
-                        power--;
-                }
-                else
-                {
-                        _putchar('0');
-                        power--;
-                }
-
+	if (n >> 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
