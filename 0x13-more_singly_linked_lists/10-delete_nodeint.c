4d138236dc50e884205be1bfa93626adc9ac6a34
Dennis Murage <dmurage140@gmail.com>
2022-11-01 10:39:11 +0000

    Handle NULL input

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *remove, *temp;
 	unsigned int i = 0;
 
-	if (head == NULL && index == 0)
+	if (*head == NULL)
 		return (-1);
 
 	if (index == 0)
