f6aba3b1cb44c84a221837e1eed6c1c640c08bbb
Dennis Murage <dmurage140@gmail.com>
2022-11-02 09:55:34 +0000

    Added README.md file and 0-read_textfile.c project file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+*read_textfile - reads a text file and prints it to the POSIX standard output.
+*
+*@filename: name of the file to read
+*@letters: number of characters to be printed
+*
+*Return: Number of characters printed
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	size_t i = 0;
+	int c;
+
+	if (filename == NULL)
+		return (0);
+
+	fp = fopen(filename, "r");
+
+	if (fp == NULL)
+		return(0);
+
+	c = fgetc(fp);
+
+	while (i < letters && c != EOF)
+	{
+		write(STDOUT_FILENO, &c, 1);
+		c = fgetc(fp);
+		i++;
+	}
+	fclose(fp);
+
+	return (i);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,3 @@
+| File name | Description |
+| :-------- | :---------- |
+| `0-read_textfile.c` | Function that reads a text file and prints it to the `POSIX` standard output. |
