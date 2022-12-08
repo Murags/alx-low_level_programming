5d048f2d1500cee5384f68ab181937e33f0356ee
Dennis Murage <dmurage140@gmail.com>
2022-12-08 14:13:06 +0000

    Hanlde error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new = add_dnodeint_end(h, n);
+		new = add_dnodeint(h, n);
 		return (new);
 	}
 	while (temp)
