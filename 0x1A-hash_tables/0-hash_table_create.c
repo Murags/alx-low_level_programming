e78943f50f2f629b228ea1239d5498fa44b10c18
Dennis Murage <dmurage140@gmail.com>
2023-01-08 15:10:13 +0000

    Error handle

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,10 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashTable;
-	unsigned int i = 0;
+	unsigned long int i = 0;
+
+	if (!size)
+		return (NULL);
 
 	hashTable =  malloc(sizeof(hash_table_t));
 	if (hashTable == NULL)
