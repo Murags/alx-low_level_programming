ac736f73cd1eaa1e82c3a1825bc2c80b46c12b37
Dennis Murage <dmurage140@gmail.com>
2023-01-08 15:52:10 +0000

    Added 3-hash_table_set.c file

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new = NULL, *tmp;
+
+
+	if (!ht || !key || !*key || !value)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(key, tmp->key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			if (!tmp->value)
+				return (0);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+
+	if (!new->value)
+	{
+		free(new);
+		return (0);
+	}
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
