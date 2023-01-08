d3da0138dc8507216850452421f5da00c0eba0f1
Dennis Murage <dmurage140@gmail.com>
2023-01-08 15:55:39 +0000

    Added 3-hash_table_set.c file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
-
+/**
+*hash_table_set - adds an element to the hash table.
+*
+*@ht: hash table to add to
+*@key: key do add
+*@value: value to add
+*
+*Return: 1 on success else 0
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
