4f90252d49e20e2776068809c57a152ea8be45e5
Dennis Murage <dmurage140@gmail.com>
2023-02-23 16:04:15 +0300

    Error handle

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,23 +15,15 @@ def ordinates(list_p, position):
   """  
   if position == "top":
     a = list_p[0] - 1
-    # if a < 0:
-    #   a = 0
     new = [a, list_p[1]]
   elif position == "bottom":
     a = list_p[0] + 1
-    # if a < 0:
-    #   a = 0
     new = [a, list_p[1]]
   elif position == "left":
     b = list_p[1] - 1
-    # if b < 0:
-    #   b = 0
     new = [list_p[0], b]
   elif position == "right":
     b = list_p[1] + 1
-    # if b < 0:
-    #   b = 0
     new = [list_p[0], b]
     
   return new
@@ -59,13 +51,25 @@ def island_perimeter(grid):
     left = ordinates(k, "left")
     right = ordinates(k, "right")
     
-    if grid[top[0]][top[1]] == 0:
+    if top[0] != -1:
+      if grid[top[0]][top[1]] == 0:
+        perimeter += 1
+    else:
       perimeter += 1
-    if grid[bottom[0]][bottom[1]] == 0:
+    if bottom[0] != -1:
+      if grid[bottom[0]][bottom[1]] == 0:
+        perimeter += 1
+    else:
       perimeter += 1
-    if grid[left[0]][left[1]] == 0:
+    if left[1] != -1:
+      if grid[left[0]][left[1]] == 0:
+        perimeter += 1
+    else:
       perimeter += 1
-    if grid[right[0]][right[1]] == 0:
+    if right[1] != -1:
+      if grid[right[0]][right[1]] == 0:
+        perimeter += 1
+    else:
       perimeter += 1
       
   return perimeter
