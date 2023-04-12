7076447fdf2895e247a18a8938d49cb05ed6dd49
Dennis Murage <dmurage140@gmail.com>
2023-04-12 15:01:06 +0300

    Fixing Styling

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,9 +1,11 @@
 #include "search_algos.h"
-/*
+/**
 * linear_search - searches for a value in an array of integers
+*
 * @array: array to search in
 * @size: size of the array
 * @value: value to look for
+*
 * Return: index of value else -1
 */
 
@@ -13,17 +15,17 @@ int linear_search(int *array, size_t size, int value)
 	int size_int = size;
 
 	if (array == NULL)
-			return (-1);
+		return (-1);
 
-	for(i = 0; i < size_int; i++)
+	for (i = 0; i < size_int; i++)
 	{
-			if (array[i] == value)
-			{
-					printf("Value checked array[%d] = [%d]\n", i, array[i]);
-					return (i);
-			}
-			else
-					printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+			return (i);
+		}
+		else
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
 	}
 
 	return (-1);
