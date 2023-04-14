fd5e1ba60cdfb136c3b7565cd8e664946361ccdd
Dennis Murage <dmurage140@gmail.com>
2023-04-14 12:37:53 +0300

    Added 100-jump.c file

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,50 @@
+#include <math.h>
+#include "search_algos.h"
+/**
+* jump_search - searches for a value in an array of integers
+*
+* @array: array to search in
+* @size: size of the array
+* @value: value to look for
+*
+* Return: index of value else -1
+*/
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t blocks, prev = 0, i;
+
+	blocks = 0;
+
+	while (blocks < size)
+	{
+		if (array[blocks] < value)
+		{
+			printf("Value checked array[%lu] = [%d]\n", blocks, array[blocks]);
+			prev = blocks;
+			blocks += sqrt(size) / 1;
+			continue;
+		}
+		else
+		{
+			printf("Value found between indexes [%lu] and [%lu]\n", prev, blocks);
+			for (i = prev; i <= blocks; i++)
+			{
+				printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+				if (array[i] == value)
+					return (i);
+			}
+		}
+	}
+	if (blocks > size)
+	{
+		printf("Value found between indexes [%lu] and [%lu]\n", prev, blocks);
+		for (i = prev; i < size; i++)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+				return (i);
+		}
+	}
+	return (-1);
+}
